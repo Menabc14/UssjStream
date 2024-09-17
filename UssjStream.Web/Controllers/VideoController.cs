a7a72a9699736702bec0a5bffd33fe702f8991b3
Emmanuel MBOUMBA NTAHINTA <ext.emboumbantahinta@cma-cgm.com>
2024-09-17 16:48:19 +0200

    adding streaming file in wwwroot

---
+++ UssjStream.Web/Controllers/VideoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UssjStream.Models;
+
+namespace UssjStream.Web.Controllers
+{
+
+    public class VideoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var video = new Video
+            {
+                Id = 1,
+                Title = "Video1",
+                VideoUrl = Url.Content($"~/videos/RM2Cesaire1.mp4")
+            };
+
+            return View(video);
+        }
+    }
+}

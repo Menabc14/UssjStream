8e4c7343ad11d27f12826552756dc05cc90112e9
Emmanuel MBOUMBA NTAHINTA <ext.emboumbantahinta@cma-cgm.com>
2024-09-17 10:08:58 +0200

    Add project models

---
+++ UssjStream.Models/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace UssjStream.Models
+{
+    public class Video
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}

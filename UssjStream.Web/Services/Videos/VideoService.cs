eee4635f2cf36262060c2b1603263c8c1929f8b7
Emmanuel MBOUMBA NTAHINTA <ext.emboumbantahinta@cma-cgm.com>
2024-09-20 10:14:13 +0200

    add service

---
+++ UssjStream.Web/Services/Videos/VideoService.cs
@@ -0,0 +1,24 @@
+﻿using UssjStream.Models;
+
+namespace UssjStream.Web.Services.Videos
+{
+    public class VideoService
+    {
+        public string GetVideoUrl(string videoName)
+        {
+            return $"~/videos/{videoName}.mp4";
+        }
+
+        public Video GetVideoById(int idVideo)
+        {
+            var video = new Video
+            {
+                Id = idVideo,
+                Title = "RM2Cesaire1",
+              
+            };
+
+            return video;
+        }
+    }
+}
